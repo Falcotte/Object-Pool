03a4d120a3ce584f16515b838b8f7792ae62d756
Ali Unal <0033484@gmail.com>
2022-09-16 17:11:50 +0300

    Demo scene adjusted

--- Assets/Demo/Scripts/PoolableCube.cs
+++ Assets/Demo/Scripts/PoolableCube.cs
@@ -5,7 +5,7 @@ public class PoolableCube : MonoBehaviour, IPoolable
 {
     public void Initialize()
     {
-
+        CubePool.Instance.ReturnToPool(this, 2f);
     }
 
     public void Terminate()
