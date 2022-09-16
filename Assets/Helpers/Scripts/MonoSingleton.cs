a48ac885d8a5787beb4d2477d148a3dc58c83ca9
Ali Unal <0033484@gmail.com>
2022-09-16 15:16:50 +0300

    MonoSingleton added

---
+++ Assets/Helpers/Scripts/MonoSingleton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<T>();
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this as T;
+        }
+    }
+}
